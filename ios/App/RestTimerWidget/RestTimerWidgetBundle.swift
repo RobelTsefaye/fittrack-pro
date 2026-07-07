9461955315586bb2b4a75756d71488b223e2ceac
BlackHammer3618 <tsefayerobel@gmail.com>
2026-07-07 12:57:26 +0200

    RestTimerWidget

---
+++ ios/App/RestTimerWidget/RestTimerWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  RestTimerWidgetBundle.swift
+//  RestTimerWidget
+//
+//  Created by Robel Tsefaye on 07.07.26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RestTimerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RestTimerWidget()
+        RestTimerWidgetControl()
+        RestTimerWidgetLiveActivity()
+    }
+}
